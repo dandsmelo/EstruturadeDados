5c1e87b1f814bd335d24d26777428802f5dad8f3
Dandara <dands20melo@gmail.com>
2023-10-03 10:00:26 -0300

    Lista Ligada

---
+++ DandsES/Atv21_09/src/MainLL.java
@@ -0,0 +1,15 @@
+public class MainLL {
+    public static void main(String[] args) {
+        ListaLigada lista1 = new ListaLigada();
+        lista1.adicionaInicio(90);
+        lista1.adicionaInicio(10);
+        System.out.println("Lista: "+lista1.toString());
+        try{
+          int valor = lista1.removeInicio();
+          System.out.println("Elemento Removido " + valor);
+          System.out.println("Lista: "+lista1.toString());
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
